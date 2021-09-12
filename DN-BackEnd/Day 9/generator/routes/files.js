b99f987bb8ec3d385f2bd9fdca10b76310f8f2a9
Himanshu Rai <raihimanshu22345gmail.com>
2021-09-12 11:54:10 +0530

    generator modified

--- DN-BackEnd/Day 9/generator/routes/files.js
+++ DN-BackEnd/Day 9/generator/routes/files.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
 const path = require("path");
-router.get("/:n", function (req, res, next) {
+router.get("/:file", function (req, res, next) {
   //   console.log(req.params);
-  res.sendFile(path.join(__dirname, "..", "public", "images", req.params.n));
+  let name = req.params.file + ".jpg";
+  res.sendFile(path.join(__dirname, "..", "public", "images", name));
 });
 
 module.exports = router;
